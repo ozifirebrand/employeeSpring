7ec18df2b5bb9263902e15130d3b9b3549fb3337
Ozioma <oziomaokoroafor@gmail.com>
2022-02-09 20:20:08 +0100

    Employee DAO

--- src/main/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepository.java
+++ src/main/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepository.java
@@ -1,2 +1,10 @@
-package africa.semicolon.employeeProgram.data.repositories;public class EmployeeRepository {
+package africa.semicolon.employeeProgram.data.repositories;
+
+import africa.semicolon.employeeProgram.data.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
