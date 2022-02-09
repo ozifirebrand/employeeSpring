dd80ec6902ab60bab0116a6ba03509af718c2f88
Ozioma <oziomaokoroafor@gmail.com>
2022-02-09 20:19:37 +0100

    Employee model

--- src/main/java/africa/semicolon/employeeProgram/data/models/Employee.java
+++ src/main/java/africa/semicolon/employeeProgram/data/models/Employee.java
@@ -1,24 +1,26 @@
 package africa.semicolon.employeeProgram.data.models;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.stereotype.Component;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
-@Component
+@Entity
 @Data
 public class Employee {
     @Id
-    @Column(nullable = false, unique = true)
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Long id;
+
     private String firstName;
     private String lastName;
     private String jobDescription;
 
+    public Long getId() {
+        return id;
+    }
 
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
