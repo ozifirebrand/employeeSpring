15b33147f914ba4af4896636e39b6b0935cbd80b
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 20:13:39 +0100

    Employee services implementation commit

--- src/main/java/africa/semicolon/employeeProgram/services/EmployeeServicesImpl.java
+++ src/main/java/africa/semicolon/employeeProgram/services/EmployeeServicesImpl.java
@@ -1,2 +1,33 @@
-package africa.semicolon.employeeProgram.services;public class EmployeeServices {
+package africa.semicolon.employeeProgram.services;
+
+import africa.semicolon.employeeProgram.data.models.Employee;
+import africa.semicolon.employeeProgram.dtos.EmployeeRequest;
+import africa.semicolon.employeeProgram.dtos.EmployeeResponse;
+
+public class EmployeeServicesImpl implements EmployeeServices {
+
+    @Override
+    public EmployeeResponse addEmployee(EmployeeRequest employeeRequest){
+        return null;
+
+    }
+    @Override
+    public void deleteEmployeeById(Long id){
+
+    }
+
+    @Override
+    public void deleteEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public EmployeeResponse getEmployeeById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EmployeeResponse getAllEmployees(Long id) {
+        return null;
+    }
 }
