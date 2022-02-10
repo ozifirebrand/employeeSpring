ee85c500e61ea86c498d026a6d4b8f4558286e66
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 10:27:39 +0100

    Employee repository commit

--- src/main/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepository.java
+++ src/main/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepository.java
@@ -2,9 +2,8 @@ package africa.semicolon.employeeProgram.data.repositories;
 
 import africa.semicolon.employeeProgram.data.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-//@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
 
 }
