a34b0304d9515e7a9dd41adf72dfcc7a7953eb56
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 10:28:25 +0100

    Test repository

--- src/test/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepositoryTest.java
+++ src/test/java/africa/semicolon/employeeProgram/data/repositories/EmployeeRepositoryTest.java
@@ -1,6 +1,7 @@
 package africa.semicolon.employeeProgram.data.repositories;
 
 import africa.semicolon.employeeProgram.data.models.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,37 +9,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Slf4j
 class EmployeeRepositoryTest {
 
     @Autowired
     private EmployeeRepository repository;
 
+    private Employee shayto;
     @BeforeEach
     void setUp() {
+        shayto = new Employee();
+        shayto.setFirstName("Shayto");
+        shayto.setLastName("Awo");
+        shayto.setJobDescription("HR");
+        repository.save(shayto);
+
 
     }
 
     @AfterEach
     void tearDown() {
+        repository.deleteAll();
     }
 
     @Test
     public void testCanAddEmployee(){
 
-        assertThat(repository).isNull();
 
-        Employee shayto = new Employee();
-        shayto.setFirstName("Shayto");
-        shayto.setLastName("Awo");
-        shayto.setJobDescription("HR");
-
-        repository.save(shayto);
-//        assertThat(repository).isNotNull();
+        assertThat(repository).isNotNull();
+        log.info("Employee id is :: {}", shayto.getId());
+        assertThat(shayto.getId()).isNotNull();
 
     }
+
+    @Test
+    public void testCanGetEmployee(){
+        Employee savedEmployee = repository.findById(shayto.getId()).get();
+        assertThat(savedEmployee.getId()).isEqualTo(shayto.getId());
+    }
+
+    @Test
+    public void testCanDeleteEmployeeRecords(){
+        log.info("Id is :: {}", shayto.getId());
+        assertThat(shayto.getId()).isNotNull();
+
+        repository.deleteEmployeeById(shayto.getId());
+        log.info("Id is :: {}", shayto.getId());
+        assertThat(shayto.getId()).isNull();
+    }
+
 
 }
