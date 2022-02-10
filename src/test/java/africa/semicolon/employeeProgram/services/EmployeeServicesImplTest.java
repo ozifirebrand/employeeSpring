6b50812b77b68ff78d8ce363a97ef36c8a14f483
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 20:14:03 +0100

    Employee test commit

--- src/test/java/africa/semicolon/employeeProgram/services/EmployeeServicesImplTest.java
+++ src/test/java/africa/semicolon/employeeProgram/services/EmployeeServicesImplTest.java
@@ -1,4 +1,38 @@
+package africa.semicolon.employeeProgram.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeServicesImplTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void deleteEmployeeById() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
 }
