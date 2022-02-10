e7f5f214f743bbfd756d18afffe204baf372b5cb
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 21:00:32 +0100

    Add employee method

--- src/main/java/africa/semicolon/employeeProgram/services/EmployeeServicesImpl.java
+++ src/main/java/africa/semicolon/employeeProgram/services/EmployeeServicesImpl.java
@@ -1,14 +1,30 @@
 package africa.semicolon.employeeProgram.services;
 
 import africa.semicolon.employeeProgram.data.models.Employee;
+import africa.semicolon.employeeProgram.data.repositories.EmployeeRepository;
 import africa.semicolon.employeeProgram.dtos.EmployeeRequest;
 import africa.semicolon.employeeProgram.dtos.EmployeeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServicesImpl implements EmployeeServices {
+
+    @Autowired
+    private EmployeeRepository repository;
 
     @Override
     public EmployeeResponse addEmployee(EmployeeRequest employeeRequest){
-        return null;
+        Employee newEmployee = new Employee();
+        newEmployee.setJobDescription(employeeRequest.getJobDescription());
+        newEmployee.setLastName(employeeRequest.getLastName());
+        newEmployee.setJobDescription(employeeRequest.getJobDescription());
+        repository.save(newEmployee);
+        EmployeeResponse response = new EmployeeResponse();
+        response.setFirstName(newEmployee.getFirstName());
+        response.setId(newEmployee.getId());
+        response.setJobDescription(newEmployee.getJobDescription());
+        return response;
 
     }
     @Override
