877722043e77bb6de1e034f539a4d008ee638974
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 21:01:27 +0100

    Employee response dto

--- src/main/java/africa/semicolon/employeeProgram/dtos/EmployeeResponse.java
+++ src/main/java/africa/semicolon/employeeProgram/dtos/EmployeeResponse.java
@@ -1,4 +1,10 @@
 package africa.semicolon.employeeProgram.dtos;
 
+import lombok.Data;
+
+@Data
 public class EmployeeResponse {
+    private Long id;
+    private String firstName;
+    private String jobDescription;
 }
