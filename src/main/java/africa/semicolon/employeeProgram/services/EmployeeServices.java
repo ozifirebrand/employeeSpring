32033a7bbf87b95642a5b9c0094a0c1ff9db53e1
Ozioma <oziomaokoroafor@gmail.com>
2022-02-10 20:12:42 +0100

    Employee services commit

--- src/main/java/africa/semicolon/employeeProgram/services/EmployeeServices.java
+++ src/main/java/africa/semicolon/employeeProgram/services/EmployeeServices.java
@@ -1,2 +1,13 @@
-package africa.semicolon.employeeProgram.services;public interface EmployeeServices {
+package africa.semicolon.employeeProgram.services;
+
+import africa.semicolon.employeeProgram.data.models.Employee;
+import africa.semicolon.employeeProgram.dtos.EmployeeRequest;
+import africa.semicolon.employeeProgram.dtos.EmployeeResponse;
+
+public interface EmployeeServices {
+    EmployeeResponse addEmployee(EmployeeRequest employeeRequest);
+    void deleteEmployeeById(Long id);
+    void deleteEmployee(Employee employee);
+    EmployeeResponse getEmployeeById(Long id);
+    EmployeeResponse getAllEmployees(Long id);
 }
