a9b023e23a1a046eb248fa069b5ed8f5555b7b55
Ozioma <oziomaokoroafor@gmail.com>
2022-02-14 21:13:53 +0100

    Going home. See ya tomorrow by the grace of GOD

--- src/main/java/africa/semicolon/employeeProgram/services/EmployeeServices.java
+++ src/main/java/africa/semicolon/employeeProgram/services/EmployeeServices.java
@@ -4,11 +4,13 @@ import africa.semicolon.employeeProgram.data.models.Employee;
 import africa.semicolon.employeeProgram.dtos.EmployeeRequest;
 import africa.semicolon.employeeProgram.dtos.EmployeeResponse;
 
+import java.util.List;
+
 public interface EmployeeServices {
     EmployeeResponse addEmployee(EmployeeRequest employeeRequest);
     void deleteEmployee(Employee employee);
     void deleteEmployeeById(Long id);
     EmployeeResponse updateEmployeeRecord(EmployeeRequest employeeRequest);
     EmployeeResponse getEmployeeById(Long id);
-    EmployeeResponse getAllEmployees(Long id);
+    List<Employee> getAllEmployees();
 }
