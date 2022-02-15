9c7923cccb0da9fe4a1a1f8daa5a2e857e4ec1b5
Ozioma <oziomaokoroafor@gmail.com>
2022-02-15 13:28:24 +0100

    Update employee details

--- src/test/java/africa/semicolon/employeeProgram/services/EmployeeServicesImplTest.java
+++ src/test/java/africa/semicolon/employeeProgram/services/EmployeeServicesImplTest.java
@@ -1,7 +1,5 @@
 package africa.semicolon.employeeProgram.services;
 
-import africa.semicolon.employeeProgram.data.models.Employee;
-import africa.semicolon.employeeProgram.data.repositories.EmployeeRepository;
 import africa.semicolon.employeeProgram.dtos.EmployeeRequest;
 import africa.semicolon.employeeProgram.dtos.EmployeeResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
@@ -32,10 +29,18 @@ class EmployeeServicesImplTest {
     void tearDown() {
     }
 
+    private EmployeeResponse createEmployee() {
+        EmployeeRequest newRequest = new EmployeeRequest();
+        newRequest.setFirstName("Shayto");
+        newRequest.setLastName("Thomas");
+        newRequest.setJobDescription("HR");
+        return services.addEmployee(newRequest);
+    }
+
     @Test
     void addEmployee() {
         EmployeeRequest request = new EmployeeRequest();
-        EmployeeResponse response = new EmployeeResponse();
+        EmployeeResponse response;
         request.setFirstName("Shayto");
         request.setLastName("Thomas");
         request.setJobDescription("HR");
@@ -60,11 +65,11 @@ class EmployeeServicesImplTest {
     @Test
     void deleteEmployee() {
         EmployeeResponse response= createEmployee();
-        Employee employee = new Employee();
+        africa.semicolon.employeeProgram.data.models.Employee employee = new africa.semicolon.employeeProgram.data.models.Employee();
         employee.setFirstName(response.getFirstName());
         employee.setJobDescription(response.getJobDescription());
         employee.setId(response.getId());
-        log.info("Employee with id {}", services.getEmployeeById(employee.getId()));
+        log.info("EmployeeResponse with id {}", services.getEmployeeById(employee.getId()));
         services.deleteEmployee(employee);
         assertThat(services.getEmployeeById(employee.getId())).isNull();
         log.info("is now {}", services.getEmployeeById(employee.getId()));
@@ -79,22 +84,28 @@ class EmployeeServicesImplTest {
         assertThat(foundResponse.getJobDescription()).isEqualTo(response.getJobDescription());
     }
 
-    private EmployeeResponse createEmployee() {
-        EmployeeRequest newRequest = new EmployeeRequest();
-        newRequest.setFirstName("Shayto");
-        newRequest.setLastName("Thomas");
-        newRequest.setJobDescription("HR");
-        return services.addEmployee(newRequest);
-    }
-
     @Test
     void getAllEmployees() {
-        List<Employee> employees = services.getAllEmployees();
+        List<africa.semicolon.employeeProgram.data.models.Employee> employees = services.getAllEmployees();
         log.info("{}",employees.size());
+        /*todo --> size changes with each test run
         assertThat(employees.size()).isEqualTo(41);
+         */
     }
 
     @Test
     void updateEmployeeRecord() {
+        EmployeeResponse response = createEmployee();
+        log.info("EmployeeResponse with id {}", services.getEmployeeById(response.getId()));
+
+        assertThat(services.getEmployeeById(response.getId())).isNotNull();
+
+        EmployeeRequest newRequest = new EmployeeRequest();
+        newRequest.setFirstName("Bola");
+        africa.semicolon.employeeProgram.data.models.Employee employee = services.updateEmployeeRecord(response.getId(), newRequest);
+        assertThat(response.getId()).isEqualTo(employee.getId());
+        assertThat(employee.getFirstName()).isEqualTo("Bola");
+        assertThat(employee.getJobDescription()).isEqualTo(response.getJobDescription());
+        assertThat(services.getEmployeeById(employee.getId())).isEqualTo(services.getEmployeeById(response.getId()));
     }
 }
