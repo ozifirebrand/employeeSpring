37fd16fa8038afb477e7e1343fde7b5983c7d227
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 17:08:33 +0100

    upload method in service impl

--- src/main/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryService.java
+++ src/main/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryService.java
@@ -1,7 +1,8 @@
 package africa.semicolon.employeeProgram.services.cloud;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
-    Map<?, ?> upload(byte[] bytes, Map<?, ?> params);
+    Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException;
 }
