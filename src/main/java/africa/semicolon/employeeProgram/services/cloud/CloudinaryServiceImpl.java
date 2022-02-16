784a36eaa2678c30356ac54c1147c5c6bc94addd
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 17:08:17 +0100

    upload method in service impl

--- src/main/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryServiceImpl.java
+++ src/main/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryServiceImpl.java
@@ -1,10 +1,18 @@
 package africa.semicolon.employeeProgram.services.cloud;
 
-import java.util.Map;
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Map;
+@Service
 public class CloudinaryServiceImpl implements CloudinaryService{
+    @Autowired
+    private Cloudinary cloudinary;
+
     @Override
-    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) {
-        return null;
+    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException {
+        return cloudinary.uploader().upload(bytes, params);
     }
 }
