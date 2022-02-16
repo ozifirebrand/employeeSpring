51aa57c5776c5c7efa3bb6620244491d551653e7
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 17:06:17 +0100

    Cloudinary upload test

--- src/test/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryServiceImplTest.java
+++ src/test/java/africa/semicolon/employeeProgram/services/cloud/CloudinaryServiceImplTest.java
@@ -1,22 +1,39 @@
 package africa.semicolon.employeeProgram.services.cloud;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.*;
+@ContextConfiguration
+@SpringBootTest
+@Slf4j
 class CloudinaryServiceImplTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    private Cloudinary cloudinary;
 
-    @AfterEach
-    void tearDown() {
-    }
+    @Autowired
+    private CloudinaryService cloudinaryService;
+
 
     @Test
-    public void upload() {
+    public void upload() throws IOException {
+        File file = new File("src/test/resources/Ozi_profile.jpg");
+        assertThat(file).isNotNull();
+        Map<?, ?> uploadedResult = cloudinaryService.upload(Files.readAllBytes(file.toPath()), ObjectUtils.asMap());
+        assertThat(uploadedResult.get("url")).isNotNull();
+        log.info("Image url is {}", uploadedResult.get("url"));
     }
 }
